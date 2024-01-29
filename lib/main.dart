2bb322867c4a8fec7cd85413bf3e062ff306c4e9
NIRAJ <nirarajput6983@gmail.com>
2024-01-29 12:59:04 +0530

    29-01-2024

---
+++ lib/main.dart
@@ -0,0 +1,328 @@
+// // import 'package:flutter/material.dart';
+// //
+// // void main() {
+// //   runApp(const MyApp());
+// // }
+// //
+// // class MyApp extends StatelessWidget {
+// //   const MyApp({super.key});
+// //
+// //   // This widget is the root of your application.
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       debugShowCheckedModeBanner: false,
+// //       title: 'Flutter Demo',
+// //       home:SafeArea(
+// //         child: Scaffold(
+// //           backgroundColor: Color(0xff7CB342),
+// //           appBar: AppBar(
+// //             centerTitle: true,
+// //             title: Text("My App"),
+// //             backgroundColor: Color(0xff8BC34A),
+// //           ),
+// //
+// //
+// //           body: Center(
+// //             child: Container(
+// //               alignment: Alignment.center,
+// //               height: 150,
+// //               width: 150,
+// //               decoration: BoxDecoration(color: Color(0xff4CAF50),
+// //               ),
+// //               child: Container(
+// //                 alignment: Alignment.center,
+// //                 height: 125,
+// //                 width: 125,
+// //                 decoration: BoxDecoration(color: Color(0xffB2FF59)
+// //                 ),
+// //                 child: Text("oooo",style: TextStyle(color: Color(0xff597F2C),fontSize: 60,letterSpacing: -19),
+// //                 ),
+// //               ),
+// //             ),
+// //           ),
+// //         ),
+// //       ),
+// //
+// //     );
+// //   }
+// // }
+//
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//     Widget build(BuildContext context) {
+//       return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'Flutter Demo',
+//         home:SafeArea(
+//           child: Scaffold(
+//             backgroundColor: Color(0xffFAFAFA),
+//             appBar: AppBar(
+//               centerTitle: true,
+//               title: Text(" Mission of RNW "),
+//               backgroundColor: Color(0xffFF5252),
+//             ),
+//
+//
+//             body: Center(
+//               child: Container(
+//                 alignment: Alignment.centerRight,
+//                 height: 40,
+//                 width: 300,
+//                 decoration: BoxDecoration(color: Color(0xffFF5252),
+//                 ),
+//                 child: Container(
+//                   alignment: Alignment.center,
+//                   height: 40,
+//                   width: 285,
+//                   decoration: BoxDecoration(color: Color(0xffFCC8C8)
+//                   ),
+//                   child: RichText(
+//                     text: const TextSpan(
+//                       children: [
+//                         TextSpan(
+//                           text: "Shapping ",style: TextStyle(color: Color(0xff221B1B),fontSize: 17.5),
+//                         ),
+//                         TextSpan(
+//                           text: " skills ",style: TextStyle(color: Color(0xff221B1B),fontSize: 15),
+//                         ),
+//                         TextSpan(
+//                           text: " for ",style: TextStyle(color: Color(0xff221B1B),fontSize: 15),
+//                         ),
+//                         TextSpan(
+//                           text: " scalling ",style: TextStyle(color: Color(0xff221B1B),fontSize: 15),
+//                         ),
+//                         TextSpan(
+//                           text: " higher \n",style: TextStyle(color: Color(0xff221B1B),fontSize: 15),
+//                         ),
+//                         TextSpan(
+//                           text: "--RNW ",style: TextStyle(color: Color(0xff221B1B),fontSize: 19),
+//                         ),
+//                       ]
+//                     ),
+//                   ),
+//
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//
+//       );
+//     }
+// }
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//     Widget build(BuildContext context) {
+//       return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: "Flutter demo",
+//         home: SafeArea(
+//           child: Scaffold(
+//           backgroundColor: Colors.white,
+//           appBar: AppBar(
+//             centerTitle: true,
+//             title: Text("Mix-Up",style: TextStyle(color: Colors.white),),
+//             backgroundColor: Color(0xffFF5252),
+//           ),
+//
+//             body: Center(
+//                child: Container(
+//                  alignment: Alignment.bottomRight,
+//                  height: 340,
+//                  width: 400,
+//                  decoration: BoxDecoration(color: Color(0xffFFFF00)),
+//
+//                   child: Container(
+//                   alignment: Alignment.bottomRight,
+//                   height: 300,
+//                   width: 310,
+//                   decoration: BoxDecoration(color: Color(0xff2196F3)),
+//
+//                    child: Container(
+//                    alignment: Alignment.topLeft,
+//                    height: 280,
+//                    width: 290,
+//                      decoration: BoxDecoration(color: Color(0xffE91E63)),
+//
+//                     child: Container(
+//                     alignment: Alignment.topLeft,
+//                     height: 250,
+//                     width: 260,
+//                     decoration: BoxDecoration(color: Color(0xffFF9800)),
+//
+//                     child: Container(
+//                     alignment: Alignment.topLeft,
+//                     height: 220,
+//                     width: 230,
+//                     decoration: BoxDecoration(color: Color(0xff4CAF50)),
+//
+//                     child: Container(
+//                     alignment: Alignment.center,
+//                     height: 200,
+//                     width: 200,
+//                     decoration: BoxDecoration(color: Color(0xff64FFDA),),
+//
+//                     ),
+//                     ),
+//                   ),
+//                 ),
+//                 ),
+//                ),
+//              ),
+//            ),
+//          ),
+//
+//        );
+//
+//
+//       }
+// }
+//
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//     Widget build(BuildContext context) {
+//       return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: "Flutter demo",
+//         home: SafeArea(
+//           child: Scaffold(
+//           backgroundColor: Colors.white,
+//           appBar: AppBar(
+//             centerTitle: true,
+//             title: Text("Letter Cover",style: TextStyle(color: Colors.white),),
+//             backgroundColor: Color(0xff4CAF50),
+//           ),
+//
+//             body: Center(
+//               child: Container(
+//                 height: 220,
+//                 width: 210,
+//                 decoration: BoxDecoration(color: Color(0xff4CAF50),
+//                 border: Border.symmetric(
+//                   horizontal: BorderSide(color: Color(0xff72C075),width: 100,),
+//
+//                   vertical: BorderSide(color: Color(0xff4CAF50),width: 80,
+//
+//                   )
+//                 ),
+//
+//               ),
+//             ),
+//            ),
+//          ),
+//         ),
+//       );
+//       }
+// }
+
+//
+//
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           backgroundColor: Colors.white,
+//           appBar: AppBar(
+//             centerTitle: true,
+//             title: Text("3D Cube",style: TextStyle(color: Colors.white),),
+//             backgroundColor: Color(0xff009688),
+//           ),
+//
+//           body: Center(
+//             child: Container(
+//               height: 300,width: 300,decoration: BoxDecoration(color: Color(0xff009688),
+//               border: Border.symmetric(horizontal: BorderSide(color: Color(0xff4db6ac),width: 50,),
+//
+//               vertical: BorderSide(color: Color(0xff33aba0),width: 60,)
+//               )
+//             ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Opened Doors",style: TextStyle(color: Colors.white),),
+            backgroundColor: Colors.black,
+          ),
+          body: Center(
+            child: Container(
+              height: 150,width: 150,decoration: BoxDecoration(color: Color(0xff000000),
+              border: Border.symmetric(
+                horizontal: BorderSide(color: Colors.black,width: 40,
+                ),
+                vertical: BorderSide(
+                  color: Color(0xffeeeeee),width: 48,
+                ),
+              ),
+            ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
